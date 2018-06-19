5d64479860a1d6300fb299585621e028d73ef9d9
Willem Franco <willemfranco@gmail.com>
2018-06-18 23:09:48 -0400

    correccion de pantalla esta o no logeado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public title: string;
+  public isLogged: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.title = 'app';
+    this.isLogged = false;
+  }
+
 }
