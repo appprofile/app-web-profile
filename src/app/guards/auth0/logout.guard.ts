33c406827b8b68434bceed6e4f54a0bf0840fe9f
Willem Franco <willemfranco@gmail.com>
2018-06-20 01:55:10 -0400

    opciones de perfiles * indentacion en guards

--- src/app/guards/auth0/logout.guard.ts
+++ src/app/guards/auth0/logout.guard.ts
@@ -5,14 +5,13 @@ import { Auth0Service } from '@services/auth0/auth0.service';
 @Injectable()
 export class LogoutGuard implements CanActivate {
 
- constructor(public auth0Service: Auth0Service, public router: Router) {}
+  constructor(public auth0Service: Auth0Service, public router: Router) {}
 
- canActivate(): boolean {
-  if (this.auth0Service.isAuthenticated()) {
-   this.router.navigate(['/profile']);
-   return false;
+  canActivate(): boolean {
+    if (this.auth0Service.isAuthenticated()) {
+      this.router.navigate(['/profile']);
+      return false;
+    }
+    return true;
   }
-  return true;
- }
-
 }
--- src/app/guards/auth0/scope.guard.ts
+++ src/app/guards/auth0/scope.guard.ts
@@ -5,15 +5,14 @@ import { Auth0Service } from '@services/auth0/auth0.service';
 @Injectable()
 export class ScopeGuard implements CanActivate {
 
- constructor(public auth0Service: Auth0Service, public router: Router) {}
+  constructor(public auth0Service: Auth0Service, public router: Router) {}
 
- canActivate(route: ActivatedRouteSnapshot): boolean {
-  const scopes = (route.data as any).expectedScopes;
-  if (!this.auth0Service.isAuthenticated() || !this.auth0Service.userHasScopes(scopes)) {
-   this.router.navigate(['/']);
-   return false;
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    const scopes = (route.data as any).expectedScopes;
+    if (!this.auth0Service.isAuthenticated() || !this.auth0Service.userHasScopes(scopes)) {
+      this.router.navigate(['/']);
+      return false;
+    }
+    return true;
   }
-  return true;
- }
-
 }
